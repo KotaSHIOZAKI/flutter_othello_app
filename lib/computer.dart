90290f2f7bfadcf864b66b3d525c273edffa86e4
KotaSHIOZAKI <ksw2270274@stu.o-hara.ac.jp>
2022-10-06 10:53:21 +0900

    COMの強さ「普通」

--- lib/computer.dart
+++ lib/computer.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'main.dart';
 import 'game_screen.dart';
 import 'options.dart';
-import 'package:flutter/material.dart';
 
 class ComputerManager {
   void comPut(int c, int r) {
@@ -17,6 +16,9 @@ class ComputerManager {
   }
 
   void comDecide() {
+    List<List<int>> posList = [];
+    List<int> countList = [];
+
     List<List<int>> maxList = [];
     int maxCount = 0;
 
@@ -41,6 +43,11 @@ class ComputerManager {
           } else if (count == minCount) {
             minList.add([c, r]);
           }
+
+          if (count > 0) {
+            posList.add([c, r]);
+            countList.add(count);
+          }
         }
       }
     }
@@ -48,15 +55,36 @@ class ComputerManager {
       minCount = 0;
     }
 
+    int i = 0;
+    while (i < countList.length) {
+      if (
+        (
+          (countList[i] == maxCount && maxCount > 2) || 
+          (countList[i] == 1 && maxCount > 1)
+        ) && countList.length > 1
+      ) {
+        posList.removeAt(i);
+        countList.removeAt(i);
+        i = 0;
+      } else {
+        i++;
+      }
+    }
+    
     int rand = 0;
     if (maxCount > 0 && comLevel == 2) {
+      //強い
       rand = Random().nextInt(maxList.length);
-      debugPrint("${maxList.length}");
 
       comPut(maxList[rand][0], maxList[rand][1]);
+    } else if ((posList.isNotEmpty && countList.isNotEmpty) && comLevel == 1) {
+      //普通
+      rand = Random().nextInt(posList.length);
+      
+      comPut(posList[rand][0], posList[rand][1]);
     } else if (minCount > 0 && comLevel == 0) {
+      //弱い
       rand = Random().nextInt(minList.length);
-      debugPrint("${minList.length}");
       
       comPut(minList[rand][0], minList[rand][1]);
     }
